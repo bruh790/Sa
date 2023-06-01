f15d64e7546c6f8a6f597855acf6ded7169e8405
bruh790 <133423002+bruh790@users.noreply.github.com>
2023-06-01 18:13:12 +0300

    Create Sa.py

---
+++ Sa.py
@@ -0,0 +1,13 @@
+import random
+a="azertyuiopqsdfghjklmwxcvbn"
+b="AZERTYUIOPQSDFGHJKLMWXCVBN"
+n="1234567890"
+s="@§%&'(--+/*="
+w=8
+g=a+b+s+n
+x=0
+while True:
+  pas="".join(random.sample(g,w))
+  print("password is: ",pas)
+  x+=1
+  x=1000
